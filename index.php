923d852ae40232b5dd6b31820e88c5035c79d336
ionvop <ionvop@gmail.com>
2025-02-21 01:11:00 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+include("common.php");
+debug();
+
+?>
+
+<html>
+    <head>
+        <title>
+            Home | ChronoZ
+        </title>
+        <base href="./">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            <?=setHeader()?>
+        </div>
+    </body>
+    <script src="script.js"></script>
+    <script>
+
+    </script>
+</html>
