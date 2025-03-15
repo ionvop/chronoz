c6d0cb588a688147e814beffb920c3cb4021ff4c
ionvop <ionvop@gmail.com>
2025-03-15 20:21:13 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -32,7 +32,7 @@ $Parsedown = new Parsedown();
         <style>
             .user {
                 display: grid;
-                grid-template-columns: max-content 1fr max-content;
+                grid-template-columns: max-content 1fr;
                 border-bottom: 0.1rem solid #fff;
             }
 
@@ -64,11 +64,55 @@ $Parsedown = new Parsedown();
                 height: 2rem;
             }
 
-            .description {
+            .panels {
+                display: grid;
+                grid-template-columns: 20rem 1fr;
+            }
+
+            .panels__user__details {
+                display: grid;
+                grid-template-columns: 1fr minmax(0, max-content) 1fr;
+                overflow: hidden;
+            }
+
+            .panels__user__details__text {
+                padding: 1rem;
+                line-height: 2rem;
+            }
+
+            .panels__user__edit {
                 padding: 1rem;
             }
 
-            .description img {
+            .panels__user__edit > svg {
+                width: 2rem;
+                height: 2rem;
+            }
+
+            .panels__user__actions {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+            }
+
+            .panels__user__action {
+                padding: 1rem;
+            }
+
+            .panels__user__action > svg {
+                width: 2rem;
+                height: 2rem;
+            }
+
+            .panels__user__actions__follow {
+                cursor: pointer;
+            }
+
+            .panels__description {
+                padding: 1rem;
+                border-left: 0.1rem solid #555;
+            }
+
+            .panels__description img {
                 max-width: 100%;
             }
         </style>
@@ -87,14 +131,50 @@ $Parsedown = new Parsedown();
                             <?=$target["username"]?>
                         </div>
                     </div>
-                    <div class="user__actions">
-                        <a class="-a user__actions__edit" href="user/edit/">
-                            <?=Icon("settings")?>
-                        </a>
-                    </div>
                 </div>
-                <div class="description">
-                    <?=$Parsedown->text($target["description"])?>
+                <div class="panels">
+                    <div class="panels__user">
+                        <div class="panels__user__details">
+                            <div></div>
+                            <div class="panels__user__details__text">
+                                Last seen: <?=TimeAgo($target["last_seen"])?><br>
+                                Followers: <br>
+                                Following:
+                            </div>
+                            <div></div>
+                        </div>
+                        <?php
+                            $personAddIcon = Icon("person_add");
+                            $flagIcon = Icon("flag");
+                            $settingsIcon = Icon("settings");
+
+                            if ($user != false) {
+                                if ($target["id"] == $user["id"]) {
+                                    echo <<<HTML
+                                        <a href="user/edit/" class="-a panels__user__edit -center">
+                                            {$settingsIcon}
+                                        </a>
+                                    HTML;
+                                } else {
+                                    echo <<<HTML
+                                        <div class="panels__user__actions">
+                                            <form action="server.php" class="-form panels__user__actions__follow panels__user__action -center" method="post" enctype="multipart/form-data" onclick="this.submit()">
+                                                {$personAddIcon}
+                                                <input type="hidden" name="username" value="{$target['username']}">
+                                                <input type="hidden" name="method" value="follow">
+                                            </form>
+                                            <a href="user/report/?id={$target['username']}" class="-a panels__user__actions__report panels__user__action -center">
+                                                {$flagIcon}
+                                            </a>
+                                        </div>
+                                    HTML;
+                                }
+                            }
+                        ?>
+                    </div>
+                    <div class="panels__description">
+                        <?=$Parsedown->text($target["description"])?>
+                    </div>
                 </div>
             </div>
         </div>
